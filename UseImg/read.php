6b4939a5b0de385c60b8d194581e178ce9d72eb0
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-26 16:20:25 +0430

    l

--- UseImg/read.php
+++ UseImg/read.php
@@ -1,3 +1,8 @@
+<?php
+include "connect.php";
+$dastor = "SELECT * FROM user_file";
+$natija =$conniction-> query($dastor)
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +11,22 @@
     <title>Document</title>
     <link rel="stylesheet" href="../src/output.css">
 </head>
+<!-- npx @tailwindcss/cli -i ./src/input.css -o ./src/output.css --watch  -->
 <body>
-    
+    <div class="h-screen  w-full p-6 flex justify-between flex-wrap overflow-y-scroll">
+    <?php  if($natija->num_rows>0){
+        while($rows=$natija->fetch_assoc()){
+        
+        ?>
+    <div class="h-[350px] w-[250px] rounded-2xl shadow-md shadow-gray-700 flex-col p-5 flex justify-center items-center">
+            <img src="./image/<?php echo $rows['imgUrl'] ?>" class="rounded-xl h-[220px] w-[220px] " alt="">
+            <h1 class="font-bold text-2xl"><?php echo $rows['name'] ?></h1>
+            <div class="flex justify-center gap-5">
+            <button class="py-2 px-4 bg-red-600 rounded-2xl">Delete</button>
+            <button class="py-2 px-4 bg-green-500 rounded-2xl">Updeat</button>
+        </div>
+        </div>
+        <?php  }} ?>
+    </div> 
 </body>
 </html>
