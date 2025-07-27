2722e3cac238c84f72b9fb4cbb5bc5eed9604830
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-27 22:18:46 +0430

    l

--- UseImg/Product.php
+++ UseImg/Product.php
@@ -6,7 +6,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="../src/output.css">
 </head>
-<body class="h-screen w-full bg-linear-180 from-40% from-gray-400 to-50% to-gray-200 via-45% via-gray-800 text-black text-center">
+<body class="h-screen w-full bg-linear-180 from-40% from-gray-400 to-50% to-gray-200 via-45% via-gray-800 text-black text-center flex justify-center ">
     
 </body>
 </html>
