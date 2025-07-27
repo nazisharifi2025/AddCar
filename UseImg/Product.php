b1e6b92de815903f7fcb993de3601e48bc47e683
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-27 22:23:41 +0430

    l

--- UseImg/Product.php
+++ UseImg/Product.php
@@ -8,7 +8,7 @@
 </head>
 <body class="h-screen w-full bg-linear-180 from-40% from-gray-400 to-50% to-gray-200 via-45% via-gray-800 text-black text-center flex justify-center items-center flex-col  rounded-2xl shadow-2xl shadow-gray-700">
     <div class="h-[80%] w-[80%] bg-gray-100 flex justify-center items-center rounded-2xl shadow-md shadow-gray-900 flex-col gap-12">
-        <img src="./image/car1.jpg" alt="">
+        <img src="./image/car1.jpg" alt="" class=" h-[70%] w-[80%]">
     </div>
 </body>
 </html>
