b6a2c86938809642ef06c0776653360b9cfb6f0c
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-27 22:21:24 +0430

    l

--- UseImg/Product.php
+++ UseImg/Product.php
@@ -7,6 +7,6 @@
     <link rel="stylesheet" href="../src/output.css">
 </head>
 <body class="h-screen w-full bg-linear-180 from-40% from-gray-400 to-50% to-gray-200 via-45% via-gray-800 text-black text-center flex justify-center items-center flex-col  rounded-2xl shadow-2xl shadow-gray-700">
-    <div class="h-[80%] w-[80%]"></div>
+    <div class="h-[80%] w-[80%] bg-gray-100 flex justify-center"></div>
 </body>
 </html>
